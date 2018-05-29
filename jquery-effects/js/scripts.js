f99f608d3fb986f0d17b72120fde1399d98452e1
Anousone Kaseumsouk and James Hanley <james@>
2018-05-29 11:35:09 -0700

    Clean up scripts.js, replace Jquery with $.

--- jquery-effects/js/scripts.js
+++ jquery-effects/js/scripts.js
@@ -1,12 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
-});
+$(document).ready(function(){
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  $("img").click(function() {
+    alert("This is an image.");
+  });
 });
