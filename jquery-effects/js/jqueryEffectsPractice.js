7290b8ef25ebf46e2e66034831643f61b7f646e0
Anousone Kaseumsouk and James Hanley <anousone@>
2018-05-29 13:20:17 -0700

    Add jqueryEffectsPractice.js to answer warmup and effects practice.

---
+++ jquery-effects/js/jqueryEffectsPractice.js
@@ -0,0 +1,12 @@
+console.log("How do we tell jQuery which part of the page to add an effect to? What does the code look like?");
+console.log("use Jquery/$ to add an id/class argument, call an event handler to trigger a functon");
+console.log("What is the DOM? How do we interact with it?");
+console.log("Document Objective Model, we interact with it by using JS manipulate elements on the page");
+console.log("When do we use an id? What about a class?");
+console.log("ID is single use, class is multiple use");
+console.log("On the walrus page, add some clickable text that alternates between saying 'Hide/show images', and toggles the images appropriately.");
+console.log("");
+console.log("On either the walrus page or the webpage from the previous exercise, try fading and sliding elements with .fadeIn(), .fadeOut(), .fadeToggle(), .slideDown(), .slideUp(), and .slideToggle().");
+console.log("");
+console.log("Make a webpage with a few different sections, where a few of the sections are hidden by default, with a bit of teaser text that expands when you click on it (e.g., 'Click to learn about the giant sandcastles of this fabulous country!'). Be creative!");
+console.log("");
